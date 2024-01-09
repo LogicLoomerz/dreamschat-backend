6053dee2b48ee8b2311716e7138ae26b9d172809
Elegant-Gracie <gracenwafor50@gmail.com>
2024-01-09 04:52:50 +0100

    feat: setup entry file and database connection

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-node_modules
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const { connectDatabase } = require("./lib/config/database");
+dotenv.config();
+
+const app = express();
+
+try {
+    connectDatabase();
+} catch (error) {
+    console.error(error)
+}
+
+app.use(cors());
+
+const port = process.env.PORT;
+
+app.get("/", async (_, res) => {
+    res.status(200).send("Welcome to DreamsChat :)");
+});
+
+app.listen(port, () => {
+    console.log(
+      `Listening on port:${port} \nVisit http://localhost:${port}/`
+    );
+  });
