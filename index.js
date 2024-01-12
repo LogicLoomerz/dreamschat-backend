273a53d8c83c0d08a28e23e43f1618886d780b74
Elegant-Gracie <gracenwafor50@gmail.com>
2024-01-12 15:46:28 +0100

    feat: add authentication endpoints

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const { connectDatabase } = require("./lib/config/database");
+const routes = require("./lib/routes/route");
+const { NOT_FOUND } = require("./lib/utils/status-codes");
 dotenv.config();
 
 const app = express();
@@ -17,8 +19,13 @@ app.use(cors());
 const port = process.env.PORT;
 
 app.get("/", async (_, res) => {
-    res.status(200).send("Welcome to DreamsChat :)");
+    res.status(200).send("Welcome to DreamsChat :)")
 });
+
+app.use("/api", routes);
+app.all("*", (_, res) => {
+    res.status(NOT_FOUND).send({message: "Route not found"});
+})
 
 app.listen(port, () => {
     console.log(
